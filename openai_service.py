55c0c23056c16a4308953b62cde7b4ab8507c16a
NguyenGPhuc <NguyenGPhuc@gmail.com>
2023-02-05 21:51:35 -0800

    progress on AI story generation with selected author and theme

--- openai_service.py
+++ openai_service.py
@@ -7,8 +7,8 @@ with open("api_key.txt", "r") as f:
     contents = f.readline().strip()
 try:
     # Althernatively, reaplce variable "contents" below with the API key. Exmaple - "123......xyz"
-    # openai.api_key = contents
-    openai.api_key = os.getenv(contents)
+    openai.api_key = contents
+    # openai.api_key = os.getenv(contents)
 except:
     print("Can't read api key from file.")
 finally:
@@ -17,16 +17,27 @@ finally:
 # uses generate text as the generated prompt for ChatGPT
 def generate_response(author, theme, text):
     try:
-        # AI generate reponse base on user specification
-        if author != 'None' and theme != 'None':
+        # AI modification of text base on user specification
+        if author != 'None' and theme != 'None' and text != '':
             prompt = "Rewrite this passage in a similar style to " + author + "in a " + theme + " theme :" + text
-        elif author == 'None' and theme != 'None':
+        elif author == 'None' and theme != 'None' and text != '':
             prompt = "Rewrite this passge in a " + theme + " theme: " + text
-        elif author != 'None' and theme == 'None':
+        elif author != 'None' and theme == 'None' and text != '':
             prompt = "Rewrite this passage in a similar style to author " + author + ": " + text
+        
         # if no specification selected, AI will just fix grammar
-        else: 
-            prompt="Correct this if it has any grammar issues, else leave it the same\n\n" + text,
+        elif text != '' and author == 'None' and theme == 'None': 
+            prompt="Correct this if it has any grammar issues, else leave it the same: " + text,
+
+        # AI generated story when there's no user input.
+        elif text != '' and author != 'None' and theme == 'None':
+            prompt = "Write a short one paragraph story similar to author " + author + " style of writing"
+        
+        elif text != '' and author == 'None' and theme != 'None':
+            prompt = "Write a short one paragraph story with " + theme + " Theme"
+        else:
+            print("Generate random story")
+            prompt = "Write a short one paragraph story about anything."
         
         completions = openai.Completion.create(
             # engine="text-davinci-002",
@@ -37,14 +48,18 @@ def generate_response(author, theme, text):
             stop=None,
             temperature=0.5,
         )
+
+        # return ChatGPT's response
+        message = completions.choices[0].text
+        return message
     # Returns error if API key is invalid
     except openai.OpenAIError as error:
         print(f"API key is invalid. Error: {error}")
         
 
-    # return ChatGPT's response
-    message = completions.choices[0].text
-    return message
+    # # return ChatGPT's response
+    # message = completions.choices[0].text
+    # return message
 
     
         
--- story_squire.py
+++ story_squire.py
@@ -16,81 +16,112 @@ paragraph_num = 1
 page_num = 1
 book_num = 1
 
+global user_input
+
 # drop down menu for authors
 def update_dropdown():
     global chosen_author
     selected_author = author_var.get()
     if selected_author == "None":
-        chosen_author = "no"
+        chosen_author = "None"
     else:
         chosen_author = selected_author
 
     global chosen_theme
     selected_theme = theme_var.get()
     if selected_theme == "None":
-        chosen_theme = "no"
+        chosen_theme = "None"
     else:
         chosen_theme =  selected_theme
-    
 
-    label.config(text="You have chosen [ " + chosen_author + " ] style with [ " + chosen_theme + " ] theme.")
+    raw_input = text_box.get("1.0", "end")
+    user_input = raw_input.strip()
+
+    # keep user up-to-date with what's happening after "Generate" button is press.
+    if user_input == '' and chosen_author != 'None' and chosen_theme != 'None':
+        label.config(text="AI have generated a story using author [ " + chosen_author + " ] style with a [ " + chosen_theme + " ] theme.")
+    elif user_input  == '' and chosen_author != 'None' and chosen_theme == 'None':
+        label.config(text="AI have generated a story using author [ " + chosen_author + " ] style with no specific theme.")
+    elif user_input == '' and chosen_author == 'None' and chosen_theme != 'None':
+        label.config(text="AI have generated a story with a ["  + chosen_theme + "] theme.")
+    elif user_input  == '' and chosen_author == 'None' and chosen_theme == 'None':
+        label.config(text="AI have generated a random story")
+    else:
+        label.config(text="Your passage was modify to match author [ " + chosen_author + " ] style with a [ " + chosen_theme + " ] theme.")
     print(f"Selected author: {selected_author}.")
     print(f"Selected theme: {selected_theme}.")
+    print(user_input)
 
 
-    
-
-# rewrite input using selected author's style
-    response = generate_response(user_input)
+    # Allow AI to either modify passage or checks its grammar
+    response = generate_response(selected_author, selected_theme, user_input)
                     
     text_box.delete("1.0", "end")
     text_box.insert("1.0", response)
 
-
-# record user input
+# save the current passage
 def save_input():
-    global user_input
-    user_input = text_box.get("1.0", "end")
+    global saved_input
+    saved_input = text_box.get("1.0", "end")
+
+# load saved passage
+def load_input():
+    if saved_input == '':
+        saved_input = "No passage was previously saved"
+
+    text_box.delete("1.0", "end")
+    text_box.insert("1.0", saved_input)
 
 # window setting
 window = tk.Tk()
 window.title("Story Squire")
-window.geometry("600x600")
-
+window.geometry("900x800")
 
 # Create drop down menu for authors
 author_options = ["None", "Edgar Allan Poe", "Hidetaka Miyazaki", "Stephen King", "H.P. Lovecraft", "George R. R. Martin", "J. R. R. Tolkien"]
 author_var = tk.StringVar()
 author_var.set("None")
 author_dropdown = tk.OptionMenu(window, author_var, *author_options)
+author_label = tk.Label(window, text="Select Author Style")
 author_dropdown.pack()
+author_label.pack()
 
 # create drop down menu for themes
-theme_options = ["None", "Fantasy", "Science Fiction", "Cthulhu Mythos", "Dark Fantasy", "Bleak", "Coming of age", "Dreams and reality", "The Supernatural"]
+theme_options = ["None", "Fantasy", "Science Fiction", "Cthulhu Mythos", "Dark Fantasy", "Romantic", "Bleak", "Coming of age", "Dreams and reality", "The Supernatural", "Comedy"]
 theme_var = tk.StringVar()
 theme_var.set("None")
 theme_dropdown = tk.OptionMenu(window, theme_var, *theme_options)
+theme_label = tk.Label(window, text="Select a theme")
 theme_dropdown.pack()
+theme_label.pack()
 
 # Create text box where user write their story
-text_box = tk.Text(window, width=60, height=20, font="Time 12")
+text_box = tk.Text(window, width=80, height=20, font="Time 12")
 text_box.pack()
 
-# Create button
-button = tk.Button(window, text="Save", command=save_input)
-button.pack()
+frame = tk.Frame(window)
+frame.pack(pady=50)
+
+
+# Create save button
+save_button = tk.Button(window, text="Save", command=save_input)
+save_button.pack()
+
+# create load button
+load_button = tk.Button(window, text="Load", command=load_input)
+load_button.pack()
 
 # Create an update button
-update_button = tk.Button(window, text="Update", command=update_dropdown)
-update_button.pack()
+update_button = tk.Button(window, text="Generate", command=update_dropdown)
+update_button.pack(padx=20)
 
 # Create a label to display the selected option
 label = tk.Label(window, text="")
 label.pack()
 
+
 window.mainloop()
 
-print(f"User input: {user_input}")
 # print(f"User author: {chosen_option}")
 
 # # program will continuously ask user for a prompt
