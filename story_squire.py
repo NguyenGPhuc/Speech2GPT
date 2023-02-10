96b7b74026e4bbb929d38e936e29d1db183c5d34
NguyenGPhuc <NguyenGPhuc@gmail.com>
2023-02-09 18:07:49 -0800

    Fixed save, load, and minor parsing problems.

--- story_squire.py
+++ story_squire.py
@@ -16,7 +16,8 @@ paragraph_num = 1
 page_num = 1
 book_num = 1
 
-global user_input
+user_input = ''
+saved_input = ''
 
 # drop down menu for authors
 def update_dropdown():
@@ -34,6 +35,9 @@ def update_dropdown():
     else:
         chosen_theme =  selected_theme
 
+    global user_input
+    if user_input == 'No passage was previously saved':
+        user_input = ''
     raw_input = text_box.get("1.0", "end")
     user_input = raw_input.strip()
 
@@ -57,15 +61,29 @@ def update_dropdown():
     response = generate_response(selected_author, selected_theme, user_input)
                     
     text_box.delete("1.0", "end")
-    text_box.insert("1.0", response)
+    text_box.insert("1.0", response.strip())
 
 # save the current passage
 def save_input():
     global saved_input
-    saved_input = text_box.get("1.0", "end")
+    saved_input = text_box.get("1.0", "end").strip()
+
+# save current pasage to file
+def save_to_page():
+    global user_input
+    word_count = 0
+    paragraph_count = 1
+    page_count = 1
+
+    file = open(os.path.joint("/output", "page_"+page_count, "x")) 
+
+
+    with open (os.path.joint("/output", "page_"+1, "w")) as file:
+        file.write(user_input)
 
 # load saved passage
 def load_input():
+    global saved_input
     if saved_input == '':
         saved_input = "No passage was previously saved"
 
@@ -87,7 +105,7 @@ author_dropdown.pack()
 author_label.pack()
 
 # create drop down menu for themes
-theme_options = ["None", "Fantasy", "Science Fiction", "Cthulhu Mythos", "Dark Fantasy", "Romantic", "Bleak", "Coming of age", "Dreams and reality", "The Supernatural", "Comedy"]
+theme_options = ["None", "High Fantasy", "Science Fiction", "Cthulhu Mythos", "Dark Fantasy", "Romance", "Bleak", "Coming of age", "Dreams and reality", "The Supernatural", "Comedy"]
 theme_var = tk.StringVar()
 theme_var.set("None")
 theme_dropdown = tk.OptionMenu(window, theme_var, *theme_options)
@@ -100,7 +118,7 @@ text_box = tk.Text(window, width=80, height=20, font="Time 12")
 text_box.pack()
 
 frame = tk.Frame(window)
-frame.pack(pady=50)
+frame.pack(pady=10)
 
 
 # Create save button
@@ -113,7 +131,7 @@ load_button.pack()
 
 # Create an update button
 update_button = tk.Button(window, text="Generate", command=update_dropdown)
-update_button.pack(padx=20)
+update_button.pack()
 
 # Create a label to display the selected option
 label = tk.Label(window, text="")
@@ -121,87 +139,3 @@ label.pack()
 
 
 window.mainloop()
-
-# print(f"User author: {chosen_option}")
-
-# # program will continuously ask user for a prompt
-# while True:
-#         # reading from speech-to-text
-#         # text_speech = speech_to_text()
-#         # print('Main Menu')
-#         # print('Say...')
-#         # print('"writing mode" to start wrting by verbally tells your story.\n')
-#         # # print('"ask ai" interact with an AI that will generate response base on user given prompt.\n')
-#         # print('"modify paragraph" to make edit to current paragraph. This can be grammar check, theme change or so on\n')
-#         # print('"modify page" to make changes to the current page. This can be grammar check, theme change or so on\n')
-#         # print('"read book" to display the currently book in its finish form.\n')
-#         promt = speech_to_text()
-
-#         # reading from user keyboard input
-#         # text_input = input('Write or say a promt\n')
-#         # promt = input('Start writing or say a promt\n')
-#         current_passage = ''
-
-#         # program will close upon hearing "close chat" or "end chat"
-#         if command_line(promt) == 'terminate':
-#             break
-
-
-#         else:
-#             # checks to see if speech was successfully converted to text
-#             if promt != "Sorry, I did not get that":
-#                 # simply record speech to text log.
-#                 if command_line(promt) == 'writing mode':
-#                     while True:
-#                         # exit writing mode
-#                         if command_line(promt) == 'close writing':
-#                             break
-#                         else:
-#                             print('Start recording your story by speaking into the mic')
-#                             promt = speech_to_text()
-#                             if command_line(promt) == 'ask ai':
-#                                 print('Ask AI anything')
-#                                 promt = speech_to_text()
-
-#                                 # program will close upon hearing "close chat" or "end chat"
-#                                 if command_line(promt) == 'close ai':
-#                                     break
-                                
-#                                 response = generate_response(promt)
-#                                 print(f"Response: {response}\n\n")
-                                
-#                                 print("Would you like to overwrite what you've written with AI generated response?")
-#                                 # overwrite user passage with AI generated resonse.
-#                                 if speech_to_text() == 'yes':
-#                                     write_log(response)
-
-#                             # basic speech to text writing mode
-#                             else:
-#                                 current_passage = promt + '. '
-
-#                                 current_paragraph = '. ' + current_passage
-
-#                                 print('Currently have: ' + current_paragraph)
-#                                 current_paragraph = check_grammar(current_paragraph)
-#                                 print('Currently have (grammar checked): ' + current_paragraph)
-
-#                 # if user want to ask openAI to do something instead of writing.
-#                 if command_line(promt) == 'ask ai':
-            
-#                     print('Ask AI anything ヽ(ヅ)ノ')
-#                     promt = speech_to_text()
-
-#                     # program will close upon hearing "close chat" or "end chat"
-#                     if command_line(promt) == 'close ai':
-#                         break
-                    
-#                     response = generate_response(promt)
-#                     print(f"Response: {response}\n\n")
-                    
-#                     print("Would you like to overwrite what you've written with AI generated response?")
-#                     # overwrite user passage with AI generated resonse.
-#                     if speech_to_text() == 'yes':
-#                         write_log(response)
-                    
-#             else:
-#                 print("Sorry, I did not get that") 
